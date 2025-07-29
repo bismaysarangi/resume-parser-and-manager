3190bb4abbe54ff3b638117a9e4bf9949df7a616
Bismay <bismaysarangi@gmail.com>
2025-07-29 20:05:00 +0530

    feat: add Badge component for UI consistency

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import { Button } from "@/components/ui/button";
+import { Badge } from "../components/ui/badge";
 
 const Home = () => {
   return (
     <div className="flex min-h-svh flex-col items-center justify-center font-[Montserrat]">
       <Button>Click me</Button>
+      <Badge>Badge</Badge>
     </div>
   );
 };
